adee2cf3e52eaab620d6c2bbdf4ede35943b49fb
jingransmith <90172899+jingransmith@users.noreply.github.com>
2022-03-15 12:58:44 +1100

    file upload- ball rolling and actuator Simulink

---
+++ used_ball_rolling_&_acuators/ball_rolling_variables.m
@@ -0,0 +1,22 @@
+%% ball rolling variables
+clc;clear all;close all;
+
+%% all in SI units
+Disk.radius = 0.003;
+Disk.length = 0.03;
+Disk.density = 1000;
+
+Floor.length = 0.5; %0.12
+Floor.width = 0.03;
+Floor.depth = 0.006;
+%Floor.angle = -10;
+
+%%
+Disk.radius = 0.2;
+Disk.length = 0.2;
+Disk.density = 1000;
+
+Floor.length = 2.5;
+Floor.width = 0.2;
+Floor.depth = 0.05;
+Floor.angle = -30;
